c3b461caae3a614c56ef129daf0324f137e947d9
ftemoura <fte.moura@campus.fct.unl.pt>
2023-05-23 12:46:30 +0100

    <3

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ public class Main {
     String[] firstLine = br.readLine().split(" ");
     int R = Integer.parseInt(firstLine[0]);
     int L = Integer.parseInt(firstLine[1]);
+    List<Pair>
         for (int i = 0; i < R; i++) {
             String[] line = br.readLine().split(" ");
             int p = Integer.parseInt(line[0]);
             int d = Integer.parseInt(line[1]);
+
         }
         for (int i = 0; i < L; i++) {
             String[] line = br.readLine().split(" ");
--- src/Problem.java
+++ src/Problem.java
@@ -4,46 +4,61 @@ import java.util.Queue;
 
 public class Problem {
 
-    public Problem() {
+    private List<Pair> edges;
+    private int numNodes;
+    private int[] populations;
+    private int[] departureCapacities;
+    private int safe;
 
+    public Problem(List<Pair> edges, int numNodes, int[] populations, int[] departureCapacities, int safe) {
+        this.edges = edges;
+        this.numNodes = numNodes;
+        this.populations = populations;
+        this.departureCapacities = departureCapacities;
+        this.safe = safe;
     }
 
     // o valor de graph[x*2][y*2] é o
-    public int solve(int[][] originalGgraph, int numNodes) {
-        for (int i = 0; i < numNodes; i++) {
-            List<Edge> edges = graph[i];
-            for (Edge e : edges) {
-
-            }
-            graph[i].contains()
-        }
+    public int solve() {
+        List<Edge>[] finalGraph = buildNetwork();
+        return edmondsKarp(finalGraph, numNodes, 0, safe);
     }
 
-    private int[][] buildNetwork(List<Integer>[] edges, int[] populations, int[] departureCapacities, int safe) {
-        int[][] network = new int[edges.length*2][edges.length*2];
-        for (int i = 0; i < edges.length; i++) {
-            List<Integer> edgesList = edges[i];
-            network[0][i] = populations[i];
-            network[i][i+edges.length] = departureCapacities[i];
-            for (int j : edgesList) {
-                network[i+edges.length][j] = Integer.MAX_VALUE;
-            }
+
+
+    private List<Edge>[] buildNetwork() {
+        List<Edge>[] finalGraph = new List<>[(numNodes*2)+1];
+        finalGraph[0] = new LinkedList<>();
+        for (int i = 1; i < numNodes; i++) {
+            if (i == safe)
+                continue;
+            if (finalGraph[i+numNodes] == null)
+                finalGraph[i+numNodes] = new LinkedList<>();
+            if (finalGraph[i] == null)
+                finalGraph[i] = new LinkedList<>();
+            finalGraph[0].add(new Edge(i, populations[i]));
+            finalGraph[i].add(new Edge(i+numNodes, departureCapacities[i]));
         }
-        return network;
+
+        for (int i = 0; i < edges.size(); i++) {
+            Pair p = edges.get(0);
+            finalGraph[p.getX()+numNodes].add(new Edge(p.getY(), Integer.MAX_VALUE));
+            finalGraph[p.getY()].add(new Edge(p.getX()+numNodes, 0));
+        }
+        return finalGraph;
     }
 
-    private int edmondsKarp(List<Edge>[] edges, int numNodes, int source, int sink) {
-        int[][] network = buildNetwork(edges, sink);
+    private int edmondsKarp(List<Edge>[] network, int numNodes, int source, int sink) {
         int[][] flow = new int[numNodes][numNodes];
-        for (Edge e : edges) {
-            flow[e.getX()][e.getY()] = 0;
+        for (int i = 0; i < network.length; i++) {
+            for (Edge e : network[i]) {
+                flow[i][e.getDestination()] = 0;
+            }
         }
         int[] via = new int[numNodes];
         int flowValue = 0;
         int increment;
-
-
-        while ( ( increment = findPath(edges, numNodes, flow, source, sink, via) ) != 0 ) {
+        while ( ( increment = findPath(network, numNodes, flow, source, sink, via) ) != 0 ) {
             flowValue += increment;
             // Update flow.
             int node = sink;
@@ -57,16 +72,36 @@ public class Problem {
     return flowValue;
 }
 
-    private int findPath(List<Edge> edges, int numNodes, int[][] flow, int source, int sink, int[] via) {
+    private int findPath(List<Edge>[] network, int numNodes, int[][] flow, int source, int sink, int[] via) {
             Queue<Integer> waiting = new LinkedList<>();
             boolean[] found = new boolean[numNodes];
-            for every Node v in network.nodes()
-            found[v] = false;
-            L[] pathIncr = new L[numNodes];
-            waiting.enqueue(source);
+            for (int i = 0; i < numNodes; i++) {
+                found[i] = false;
+            }
+            int[] pathIncr = new int[numNodes];
+            waiting.add(source);
             found[source] = true;
             via[source] = source;
-            pathIncr[source] = ;
+            pathIncr[source] = Integer.MAX_VALUE;
+
+            do {
+                int origin = waiting.remove();
+                for (int i = 0; i < network.length; i++) {
+                    for (Edge e : network[i]) {
+                        int destin = e.getDestination();
+                        int residue = e.getCost() - flow[i][e.getDestination()];
+                        if (!found[destin] && residue > 0) {
+                            via[destin] = origin;
+                            pathIncr[destin] = Math.min(pathIncr[origin], residue);
+                            if (destin == sink)
+                                return pathIncr[destin];
+                            waiting.add(destin);
+                            found[destin] = true;
+                        }
+                    }
+                }
+            } while ( !waiting.isEmpty() );
+            return 0;
     }
 }
 
